04b619bc2ed31ba95f7810a81d05874ee29b1074
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-05-03 14:30:12 -0300

    Delete 2023-04-blueberry directory

--- 2023-04-blueberry/blueberry-core/contracts/interfaces/curve/ICurveGaugeController.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.16;
-
-interface ICurveGaugeController {
-    function gauges(uint256) external view returns (address);
-}
--- 2023-04-blueberry/blueberry-core/contracts/interfaces/uniswap/IUniswapV2Router02.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.16;
-
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
